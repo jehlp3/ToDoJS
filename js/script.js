983a79cefa7e0481ff7f51c620988126ac3ebe22
jehlp3 <jessicalacerdap@hotmail.com>
2024-01-28 17:37:35 -0300

    Atualização do projeto

--- js/script.js
+++ js/script.js
@@ -2,33 +2,104 @@
 const todoForm = document.querySelector("#todo-form");//shit+alt+seta p baixo p criar vários
 const todoInput = document.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
-const editForm = document.querySelector("#edit-list");
+const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+let oldInputValue;
 
 //funções
 const saveTodo = (text) => {
-    const todo=document.createElement("div")
-    todo.classList.add("todo")
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
 
-    const todoTitle = document.createElement("h3")
+    const todoTitle = document.createElement("h3");
     todoTitle.innerText = text;
-    todo.appendChild
-}
+    todo.appendChild(todoTitle);
 
-//eventos
-todoForm.addEventListener("submit", (e) =>{
+    const doneBtn = document.createElement("button");
+    doneBtn.innerHTML = '<i class="fa-solid fa-check finish-todo"></i>';
+    todo.appendChild(doneBtn);
+
+    const editBtn = document.createElement("button");
+    editBtn.innerHTML = '<i class="fa-solid fa-pen edit-todo"></i>';
+    todo.appendChild(editBtn);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark edit-todo"></i>';
+    todo.appendChild(deleteBtn);
+
+    todoList.appendChild(todo);
+
+    todoInput.value = "";
+    };
+
+const toggleForms = () => {
+    editForm.classList.toggle("hide");
+    todoForm.classList.toggle("hide");
+    todoList.classList.toggle("hide");
+};
+
+const updateTodo = (text) => {
+    const todos = document.querySelectorAll(".todo");
+
+    todos.forEach((todo) => {
+        let todoTitle = todo.querySelector("h3");
+
+        if (todoTitle.innerText === oldInputValue) {
+            todoTitle.innerText = text;
+        }
+    });
+};
+
+// Eventos
+todoForm.addEventListener("submit", (e) => {
     e.preventDefault();
-   // console.log("Enviou form")
-   //adicionou a tarefa
+    
+    const inputValue = todoInput.value;
 
-   const inputValue = todoInput.value; //nome da tarefa
+    if (inputValue) {
+        //console.log(inputValue);
+        saveTodo(inputValue);
+    }
+});
 
-   if(inputValue){
-    //console.log(inputValue);
-    saveTodo(inputValue)
-   }
-}
+document.addEventListener("click", (e) => {
+    const targetEl = e.target;
+    const parentEl = targetEl.closest("div");
 
+    if (targetEl.classList.contains("finish-todo")) {
+        parentEl.classList.toggle("done");
+    }
 
-)
+    if (targetEl.classList.contains("remove-todo")) {
+        parentEl.remove();
+    }
+
+    if (targetEl.classList.contains("edit-todo")) {
+        const todoTitle = parentEl.querySelector("h3").innerText;
+
+        toggleForms();
+
+        editInput.value = todoTitle;
+        oldInputValue = todoTitle;
+    }
+});
+
+cancelEditBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    toggleForms();
+});
+
+editForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const editInputValue = editInput.value;
+
+  console.log(editInputValue);
+
+    if (editInputValue) {
+        updateTodo(editInputValue);
+    }
+
+    toggleForms();
+});
